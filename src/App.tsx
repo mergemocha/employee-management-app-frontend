c98d46fd51f344c355ee5929f6f3b55f1c667930
Anna Smati <anna.smati@gmail.com>
2022-03-08 11:28:01 +0200

    Remove component test render from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import './assets/scss/App.scss'
-import Login from './components/Login'
 
 function App (): JSX.Element {
   return (
-    <div className='App'>
-      <Login />
-    </div>
+    <div className='App' />
   )
 }
 
