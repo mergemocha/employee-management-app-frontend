ca7839a2af0a38d38028692493b7d012468b299b
Linus Willner <18148938+linuswillner@users.noreply.github.com>
2022-03-09 12:06:39 +0200

    Add frontend routing (#12)
    * Add router
    
    * Fix tests

---
+++ src/router/routes.ts
@@ -0,0 +1,24 @@
+import { lazy, LazyExoticComponent } from 'react'
+
+export interface Route {
+  path: string
+  component: LazyExoticComponent<() => JSX.Element>
+  ignoreAuth?: boolean
+}
+
+const routes: { [key: string]: Route } = {
+  login: {
+    path: '/login',
+    component: lazy(async () => await import('../views/Login'))
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  routes.loading = {
+    path: '/loading',
+    component: lazy(async () => await import('../views/Loading')),
+    ignoreAuth: true
+  }
+}
+
+export default routes
